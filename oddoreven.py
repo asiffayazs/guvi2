080564c0c66eb189b516f00e2738b214bdf4c59a
Asif Fayaz <35559365+havoc007@users.noreply.github.com>
2019-02-07 11:33:54 +0530

    Delete oddoreven.py

--- oddoreven.py
+++
@@ -1,9 +0,0 @@
-def main():
-  a=int(raw_input("Enter the number"))
-  if(a>0):
-    if(a%2==0):
-      print("The number is Even")
-    else:
-      print("The number is Odd")
-  else:
-    print("The number is invalid")
