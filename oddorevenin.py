5ac487780ba67f9a14dd6f581b7c427b69b392f7
Asif Fayaz <35559365+havoc007@users.noreply.github.com>
2019-02-07 11:40:13 +0530

    Create oddorevenin.py

---
+++ oddorevenin.py
@@ -0,0 +1,9 @@
+def main():
+  x=int(raw_input("enter the number"))
+  if(x>0):
+    if(x%2==0):
+      print("the number is even ")
+    else:
+      print("the number is odd")
+    else:
+      print("the number is invalid")
